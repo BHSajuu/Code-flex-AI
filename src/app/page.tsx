4a7f2f5c177f88b804b6da896f373dcabbf3b662
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-06-04 05:55:48 +0530

    authentication  done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+"use client"
+
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <p className="mt-4">Welcome to the home page!</p>
+      <SignedIn>
+        <SignOutButton/>  
+      </SignedIn>
+      <SignedOut>
+       <SignInButton/>  
+      </SignedOut>      
+    </div>
+
   )
 }
 
