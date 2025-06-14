b1a00456674a6492574569f03cca968c3ed7fdc1
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-06-14 11:04:48 +0530

    Setting Up Our Database

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+      domain: "https://normal-caribou-37.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
